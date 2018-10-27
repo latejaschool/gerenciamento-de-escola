2a8ec6a54d6ad5e709a1fc56824950166bdc49a3
alessandrofeitoza <eu@alessandrofeitoza.eu>
2018-10-27 18:02:58 -0300

    feature: Dashboard com quantitativos de cadastros

--- src/Controller/IndexController.php
+++ src/Controller/IndexController.php
@@ -7,12 +7,30 @@
 namespace App\Controller;
 
 use App\Controller\ControllerInterface\ControllerInterface;
+use App\Repository\CourseRepository;
+use App\Repository\StudentRepository;
+use App\Repository\TeacherRepository;
 
 final class IndexController extends Controller implements ControllerInterface
 {
+    private $courseRepository;
+    private $studentRepository;
+    private $teacherRepository;
+
+    public function __construct()
+    {
+        $this->courseRepository = new CourseRepository();
+        $this->studentRepository = new StudentRepository();
+        $this->teacherRepository = new TeacherRepository();
+    }
+
     public function index()
     {
-        $this->render('index/index');
+        $this->render('index/index', [
+            'students' => $this->studentRepository->count([]),
+            'teachers' => $this->teacherRepository->count([]),
+            'courses' => $this->courseRepository->count([]),
+        ]);
     }
 
     public function login()
